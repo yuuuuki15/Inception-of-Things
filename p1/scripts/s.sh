4c165ea5c9b7c5ee865b9bbd4132b0077b586fd9
yuuuuki15 <rx86340616@gmail.com>
2025-05-26 18:12:56 +0200

    [update] shell scripts. - check token - save path and aliases to /etc/profile.d directory

--- p1/scripts/s.sh
+++ p1/scripts/s.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-
+echo "[INFO]  setting up k3s server"
 sudo apt update -y
 sudo apt install -y curl
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s -
+echo "[INFO]  installing k3s server"
+export INSTALL_K3S_EXEC="server --flannel-iface=eth1 --write-kubeconfig-mode 644"
+curl -sfL https://get.k3s.io | sh -s -
+echo "[INFO]  k3s server installed"
 
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/shared/node-token
+echo "[INFO]  node-token copied to /vagrant/shared/node-token"
 
-echo PATH=$PATH >> /etc/default/k3s
+echo 'alias k="kubectl"' | sudo tee /etc/profile.d/k3s-aliases.sh
+sudo chmod +x /etc/profile.d/k3s-aliases.sh
+
+sudo apt install -y net-tools
+export PATH=${PATH}:/sbin
+
+echo "PATH=$PATH" >> /etc/profile.d/k3s-path.sh
