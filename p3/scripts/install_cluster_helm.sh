65fc88046de437db33d8a5188a057ee2db81fb95
mboivin <41596925+matboivin@users.noreply.github.com>
2025-06-16 20:49:11 +0200

    [wip] Continue part 3

--- p3/scripts/install_cluster_helm.sh
+++ p3/scripts/install_cluster_helm.sh
@@ -5,14 +5,28 @@
 set -e
 
 # Create cluster
-sudo k3d cluster create p3 --agents 2
+sudo k3d cluster create p3 --agents 2 -p "8080:80@loadbalancer"
 # Create namespaces
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
 
 # Add Argo CD Helm repository
-helm repo add argo https://argoproj.github.io/argo-helm
+sudo helm repo add argo https://argoproj.github.io/argo-helm
 # Update information of available charts locally from chart repositories
-helm repo update
+sudo helm repo update
 # Install Argo CD
-helm install argocd argo/argo-cd --namespace argocd
+sudo helm install argocd argo/argo-cd --namespace argocd
+
+# Wait for argocd server to be ready
+while true; do
+    number_of_pods=$(sudo kubectl get pods -n argocd | awk '{print $3}' | grep "Running" | wc -l)
+    if [ $number_of_pods = "7" ]; then
+        echo "Argocd server is ready."
+        break
+    fi
+    echo "($number_of_pods/7) Waiting for Argo CD server to be ready..."
+    sleep 5
+done
+
+# Create Ingress
+sudo kubectl apply -f manifests/ingress.yaml --namespace argocd
