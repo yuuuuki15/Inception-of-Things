83f4990decd340c5e53ef2432293894e9b512d13
yuuuuki15 <rx86340616@gmail.com>
2025-06-18 17:40:10 +0200

    [add] por-forwarding

--- p3/scripts/dev.sh
+++ p3/scripts/dev.sh
@@ -4,3 +4,5 @@ set -e
 
 sudo kubectl apply -f configs/dev/app.yaml --namespace argocd
 sudo kubectl apply -f configs/dev/ingress.yaml --namespace dev
+
+sudo kubectl port-forward service/playground -n dev 8888:8888 &
