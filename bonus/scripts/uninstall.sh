4f8b3043c52d20f6e9954183c0f656054c075601
yuuuuki15 <rx86340616@gmail.com>
2025-06-28 12:02:15 +0200

    [add] remove argocd and gitlab DNS entry in uninstall.sh

--- bonus/scripts/uninstall.sh
+++ bonus/scripts/uninstall.sh
@@ -2,3 +2,6 @@
 
 # destroy cluster
 sudo k3d cluster delete p3
+
+# remove argocd and gitlab DNS entry from /etc/hosts(create backup named /etc/hosts.bak)
+sudo sed -i.bak -e '/^127\.0\.0\.1\s\+argocd\.local$/d' -e '/^127\.0\.0\.1\s\+gitlab\.local$/d' /etc/hosts
